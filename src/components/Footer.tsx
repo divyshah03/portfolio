9a581081dad6a234814e4363649bca32633c0cb6
Divy Shah <divyrshah3@gmail.com>
2025-09-09 23:32:21 +0530

    feat: create footer with email and auto-updating copyright

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+export default function Footer() {
+  return (
+    <footer id="footer">
+      <div className="container">
+        <a href="mailto:communitypro47@gmail.com">communitypro47@gmail.com</a>
+
+        <p>
+          Copyright &copy; Communitypro {new Date().getFullYear()}, All rights reserved
+        </p>
+      </div>
+    </footer>
+  )
+}
